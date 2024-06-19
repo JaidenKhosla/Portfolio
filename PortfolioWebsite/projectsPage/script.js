1411c2cb41370500bc9fe5134574b722e2c7b0b1
Jaiden Khosla <jaiden@khosla.io>
2024-06-18 23:47:54 -0400

    added dyanmic system of adding projects(blog kind of system) reorganized some files

--- PortfolioWebsite/projectsPage/script.js
+++ PortfolioWebsite/projectsPage/script.js
@@ -1,10 +1,26 @@
+//adding projects!
+let id = 0;
+
 function addProject(project){
-    const { Name, description, link, img } = project;
+    console.log(project);
+    id++;
+    document.body.innerHTML+=`
+                <div class="project" id="${id}">
+                <div class="textContainer">
+                    <h1>${project.projectName}</h1>
+                    <p>${project.projectDescription}</p>
+                       <a href="${project.projectLink}" target="_blank">Link to Project!</a><br>
+                </div>
+                <div class="imageContainer">
+                    <img src="${project.projectImg}">
+                </div>
+            </div>
+    `;
 }
 
 addProject({
     projectName: "Calculator",
-    projectDescription: "My first Javascript project! Inspired by the apple calculator.",
+    projectDescription: "My first Javascript project!<br> Inspired by the apple calculator.",
     projectLink: "https://rawcdn.githack.com/TheBrainyCat/Portfolio/bf6d3253c710ff3bd5213e00d76fd67574725f8e/PortfolioWebsite/Projects/calculator/index.html",
-    projectImg /*String of the file reference*/: ""
+    projectImg /*String of the file reference*/: "projectGIFS/calculatorGif.gif"
 });
